a670700f7c0015e02066e426ba697897edd4fc1b
Palapeli <26661008+mkwcat@users.noreply.github.com>
2025-09-10 11:35:54 -0400

    nw4r::snd::SoundArchiveFile: Fix some incorrect Toadette sounds

--- source/nw4r/snd/SoundArchiveFile.cpp
+++ source/nw4r/snd/SoundArchiveFile.cpp
@@ -3,6 +3,7 @@
 
 #include "SoundArchiveFile.h"
 
+#include <framework/f_sound_id.h>
 #include <revolution/os.h>
 
 namespace nw4r::snd::detail
@@ -21,7 +22,37 @@ static bool IsToadetteSound(u32 id)
 static u32 GetBaseToadetteSoundId(u32 id)
 {
     if (IsToadetteSound(id)) {
-        return id - (5010 - 1010);
+        if (id == SE_VOC_KC_CS_COURSE_MISS) {
+            return SE_VOC_KO_CS_COURSE_MISS;
+        }
+        if (id == SE_VOC_KC_CONTINUE) {
+            // TODO: There's more to this one
+            return SE_VOC_KO_CONTINUE;
+        }
+        if (id == SE_VOC_KC_YOSHI_JUMP) {
+            // TODO: There's a wait on this one that shouldn't exist
+            return SE_VOC_KO2_CANNON_SHOT_S;
+        }
+        if (id == SE_VOC_KC_DAMAGE_EATEN) {
+            return SE_VOC_KO_DAMAGE_EATEN;
+        }
+        if (id == SE_VOC_KC_GET_STAR) {
+            // TODO: Use com_3wave_r instead of ma_3wave_r
+            return SE_VOC_MA_GET_STAR;
+        }
+        if (id == SE_VOC_KC_CLEAR_LAST_BOSS) {
+            return SE_VOC_KO_CLEAR_LAST_BOSS;
+        }
+        if (id == SE_VOC_KC_CS_COMPLETE_DEMO) {
+            return SE_VOC_KO_CS_COMPLETE_DEMO;
+        }
+        if (id == SE_VOC_KC_MISS_PRINCESS) {
+            // TODO: Wait time is different
+            return SE_VOC_KO_MISS_PRINCESS;
+        }
+        // TODO: SE_VOC_KC_GAME_OVER, SE_VOC_KC_TIMEUP_MULTI, SE_VOC_KC_CLEAR_MULTI
+
+        return id - (SE_VOC_KC_PLAYER_JOIN - SE_VOC_KO2_PLAYER_JOIN);
     }
     return id;
 }
