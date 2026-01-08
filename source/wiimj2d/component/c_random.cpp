2d73126414bb28bc47ab09b095721b9671225173
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-01-08 18:20:14 -0500

    dNextGotoSort_c: Import from pipe randomizer

--- source/wiimj2d/component/c_random.cpp
+++ source/wiimj2d/component/c_random.cpp
@@ -1,2 +1,7 @@
 // c_random.cpp
 // NSMBW .text: 0x801615A0 - 0x80161670
+
+#include "c_random.h"
+
+[[address(0x801615A0)]]
+u32 cRnd_c::next(u32 limit);
---
+++ source/wiimj2d/component/c_random.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// @unofficial
+class cRnd_c
+{
+public:
+    // Instance Methods
+    // ^^^^^^
+
+    /* 0x801615A0 */
+    u32 next(u32 limit);
+
+public:
+    // Instance Variables
+    // ^^^^^^
+
+    /* 0x0 */ u32 mSeed;
+};
